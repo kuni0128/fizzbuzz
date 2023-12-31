b352270ef94920f67e4c50aee788617cd1af8f0e
kuniaki <k.ishikawa.0128@gmail.com>
2023-12-31 17:48:51 +0900

    fix: fizz buzz app test

--- src/app/FizzBuzzSequencePrinter.php
+++ src/app/FizzBuzzSequencePrinter.php
@@ -3,20 +3,20 @@
 namespace FizzBuzz\App;
 
 use FizzBuzz\Core\NumberConverter;
-use FizzBuzz\Spec\CyclicNumberRule;
-use FizzBuzz\Spec\PassThroughRule;
 
 class FizzBuzzSequencePrinter
 {
+    public function __construct(
+        protected NumberConverter $fizzBuzz,
+        protected OutputInterface $output,
+    ) {
+    }
+
     public function printRange(int $begin, int $end): void
     {
-        $fizzBuzz = new NumberConverter([
-            new CyclicNumberRule(3, 'Fizz'),
-            new CyclicNumberRule(5, 'Buzz'),
-            new PassThroughRule(),
-        ]);
         for ($i = $begin; $i <= $end; $i++) {
-            printf("%d %s\n", $i, $fizzBuzz->convert($i));
+            $formattedText = sprintf("%d %s\n", $i, $this->fizzBuzz->convert($i));
+            $this->output->write($formattedText);
         }
     }
 }
